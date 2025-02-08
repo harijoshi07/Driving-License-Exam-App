96b2d0ed1366c82a816abb7dea9e1b1d02fca842
harijoshi <hariojoshi@gmail.com>
2025-02-08 20:31:11 +0545

    style: add splash screen

--- app/src/main/java/com/example/drivinglicenseexamnepal_/MainActivity.kt
+++ app/src/main/java/com/example/drivinglicenseexamnepal_/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.drivinglicenseexamnepal_.navigation.AppNavigation
 import com.example.drivinglicenseexamnepal_.ui.theme.DrivingLicenseExamNepal_Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         enableEdgeToEdge()
+
         setContent {
             DrivingLicenseExamNepal_Theme {
 
